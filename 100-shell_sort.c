251f31aef59f6e9cc5e16782e004af997f11f2a4
el-dani-cortes <danicor83@gmail.com>
2021-02-01 22:59:11 +0000

    Added a shell sort algorithm

---
+++ 100-shell_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+ * shell_sort - Shell sort algorithm
+ *
+ * @array: array with numbers to sort
+ * @size: size of the array
+ *
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t h = 0, i, j;
+	int value;
+
+	/*Calculate the gap*/
+	while (h < size / 3)
+		h = h * 3 + 1;
+	while (h > 0)
+	{
+		for (j = h; j < size; j++)
+		{
+			value = array[j];
+			i = j;
+			while (i > h - 1 && array[i - h] >= value)
+			{
+				array[i] = array[i - h];
+				i = i - h;
+			}
+			array[i] =  value;
+		}
+		print_array(array, size);
+		h = (h - 1) / 3;
+	}
+}
