ed53f94fa66637d2438fa33d819b8e62caedc4aa
el-dani-cortes <danicor83@gmail.com>
2021-02-01 10:44:18 +0000

    Add function that does sort an array of numbers by bubble sort algorithm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Bubble sort algorithm
+ *
+ * @array: The array to be sorted
+ * @size: Number of elements in @array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	unsigned long int j, _switch = 1;
+	int tmp = 1;
+
+	while (_switch)
+	{
+		_switch = 0;
+		for (j = 1; j <= size - 1; j++)
+		{
+			if (array[j] < array[j - 1])
+			{
+				_switch = 1;
+				tmp = array[j - 1];
+				array[j - 1] = array[j];
+				array[j] = tmp;
+				print_array(array, size);
+			}
+		}
+
+	}
+}
