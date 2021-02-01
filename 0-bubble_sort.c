62329376abb3e678fd597704397178fbb4023b70
el-dani-cortes <danicor83@gmail.com>
2021-02-01 13:38:49 +0000

    Chnaged the for condition and changed the position call of thw switch

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -14,15 +14,15 @@ void bubble_sort(int *array, size_t size)
 	while (_switch)
 	{
 		_switch = 0;
-		for (j = 1; j <= size - 1; j++)
+		for (j = 1; j < size; j++)
 		{
 			if (array[j] < array[j - 1])
 			{
-				_switch = 1;
 				tmp = array[j - 1];
 				array[j - 1] = array[j];
 				array[j] = tmp;
 				print_array(array, size);
+				_switch = 1;
 			}
 		}
 
