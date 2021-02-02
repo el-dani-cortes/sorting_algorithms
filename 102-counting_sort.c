a2884929a23adb1bce5dbb22168b40c5df4dfde8
el-dani-cortes <danicor83@gmail.com>
2021-02-02 19:36:57 +0000

    Added a switch to evaluate when is already sorted

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -10,25 +10,24 @@
 void counting_sort(int *array, size_t size)
 {
 	size_t i;
-	int max = 0, position;
-	int *count = NULL, *order = NULL;
+	int max, position, _switch = 0, *count = NULL, *order = NULL;
 
 	if (size == 0 || array == NULL)
 		return;
 	/*Find the maximun value in the array*/
+	max = array[0];
 	for (i = 0; i < size; i++)
 		if (array[i] > max)
-			max = array[i];
+			max = array[i], _switch = 1;
+	if (_switch == 0)
+		return;
 	/*Use malloc(size of the max value) to create a new array for the count*/
 	count = malloc(sizeof(int) * (max + 1));
 	for (i = 0; i <= (size_t)max; i++)
 		count[i] = 0;
 	/*Save the counter values of every number in the count array*/
 	for (i = 0; i < size; i++)
-	{
-		position = array[i];
-		count[position] = count[position] + 1;
-	}
+		count[array[i]] = count[array[i]] + 1;
 	/*Sum values in the count array*/
 	for (i = 1; i <= (size_t)max; i++)
 		count[i] = count[i] + count[i - 1];
