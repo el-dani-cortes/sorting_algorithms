b9c64516504fe30f570bc0d3c2a6e3c50f3de74d
el-dani-cortes <danicor83@gmail.com>
2021-02-02 19:04:37 +0000

    Added a cast to the max variable

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -10,7 +10,7 @@
 void counting_sort(int *array, size_t size)
 {
 	size_t i;
-	int max = 0, key, position;
+	int max = 0, position;
 	int *count = NULL, *order = NULL;
 
 	/*Find the maximun value in the array*/
@@ -19,12 +19,9 @@ void counting_sort(int *array, size_t size)
 		if (array[i] > max)
 			max = array[i];
 	}
-	/*Define key value*/
-	key = max;
-
-	/*Use malloc(size of the key value) to create a new array for the count*/
-	count = malloc(sizeof(int) * (key + 1));
-	for (i = 0; i <= key; i++)
+	/*Use malloc(size of the max value) to create a new array for the count*/
+	count = malloc(sizeof(int) * (max + 1));
+	for (i = 0; i <= (size_t)max; i++)
 		count[i] = 0;
 	/*Save the counter values of every number in the count array*/
 	for (i = 0; i < size; i++)
@@ -33,9 +30,9 @@ void counting_sort(int *array, size_t size)
 		count[position] = count[position] + 1;
 	}
 	/*Sum values in the count array*/
-	for (i = 1; i <= key; i++)
+	for (i = 1; i <= (size_t)max; i++)
 		count[i] = count[i] + count[i - 1];
-	print_array(count, key + 1);
+	print_array(count, max + 1);
        /*use malloc(same size of array) to create array to sort the numbers*/
 	order = malloc(sizeof(int) * size);
 	for (i = 0; i < size; i++)
